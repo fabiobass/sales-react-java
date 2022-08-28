b8b372eb916af75908b248da749e922d00b28d84
Fabio Silva <fsilva.developer@gmail.com>
2022-08-27 23:33:38 -0300

    SalesTable component

---
+++ front-web/src/components/SalesTable/index.tsx
@@ -0,0 +1,90 @@
+import './styles.css';
+
+function SalesTable() {
+  return (
+    <div className="sales-table-container base-card">
+      <h3 className="sale-table-title">Vendas recentes</h3>
+      <table className="sales-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Data</th>
+            <th>Gênero</th>
+            <th>Categorias</th>
+            <th>Loja</th>
+            <th>Forma de pagamento</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>31/08/1979</td>
+            <td>Masculino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>31/08/1979</td>
+            <td>Masculino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>31/08/1979</td>
+            <td>Masculino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>31/08/1979</td>
+            <td>Masculino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>31/08/1979</td>
+            <td>Masculino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>31/08/1979</td>
+            <td>Masculino</td>
+            <td>Roupas e acessórios</td>
+            <td>São Paulo</td>
+            <td>Crédito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SalesTable;
