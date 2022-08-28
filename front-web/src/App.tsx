7572432da8599e10b7a9582b5e1732fe04c6a0a8
Fabio Silva <fsilva.developer@gmail.com>
2022-08-27 22:21:33 -0300

    PieChartCard component

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import Filter from './components/Filter';
 import Header from './components/Header';
+import PieChartCard from './components/PieChartCard';
 import SalesByDate from './components/SalesByDate';
 import SalesSummary from './components/SalesSummary';
 
@@ -13,6 +14,16 @@ function App() {
         <SalesByDate />
         <div className="sales-overview-container">
           <SalesSummary />
+          <PieChartCard
+            name="Lojas"
+            labels={['São Paulo', 'Uberlândia', 'Alagoas']}
+            series={[40, 30, 30]}
+          />
+          <PieChartCard
+            name="Pagamento"
+            labels={['Crédito', 'Débito', 'Dinheiro']}
+            series={[20, 50, 30]}
+          />
         </div>
       </div>
     </>
