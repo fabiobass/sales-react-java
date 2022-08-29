3f55c73418773195b70efcaaad79a37e235778d9
Fabio Silva <fsilva.developer@gmail.com>
2022-08-28 22:30:09 -0300

    Finish backend integration

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,15 +1,45 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { FilterData, PieChartConfig, SalesByPaymentMethod, SalesByStore } from './types';
+import { buildFilterParams, makeRequest } from './utils/request';
+import { buildSalesByPaymentMethod, buildSalesByStoreChart } from './helpers';
 import Filter from './components/Filter';
 import Header from './components/Header';
 import PieChartCard from './components/PieChartCard';
 import SalesByDate from './components/SalesByDate';
 import SalesSummary from './components/SalesSummary';
 import SalesTable from './components/SalesTable';
-import { FilterData } from './types';
 
 function App() {
   const [filterData, setFilterData] = useState<FilterData>();
+  const [salesByStore, setSalesByStore] = useState<PieChartConfig>();
+  const [salesByPaymentMethod, setSalesByPaymentMethod] = useState<PieChartConfig>();
+  const params = useMemo(() => buildFilterParams(filterData), [filterData]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesByStore[]>('/sales/by-store', { params })
+      .then((response) => {
+        const newSalesByStore = buildSalesByStoreChart(response.data);
+        setSalesByStore(newSalesByStore);
+      })
+      .catch(() => {
+        console.error('Error to fetch sales by store');
+      });
+  }, [params]);
+
+  useEffect(() => {
+    makeRequest
+      .get<SalesByPaymentMethod[]>('/sales/by-payment-method', { params })
+      .then((response) => {
+        const newSalesByPaymentMethod = buildSalesByPaymentMethod(response.data);
+        setSalesByPaymentMethod(newSalesByPaymentMethod);
+      })
+      .catch(() => {
+        console.error('Error to fetch sales by payment method');
+      });
+  }, [params]);
+
   const onFilterChange = (filter: FilterData) => {
     setFilterData(filter);
   };
@@ -21,19 +51,15 @@ function App() {
         <Filter onFilterChange={onFilterChange} />
         <SalesByDate filterData={filterData} />
         <div className="sales-overview-container">
-          <SalesSummary />
-          <PieChartCard
-            name="Lojas"
-            labels={['São Paulo', 'Uberlândia', 'Alagoas']}
-            series={[40, 30, 30]}
-          />
+          <SalesSummary filterData={filterData} />
+          <PieChartCard name="Lojas" labels={salesByStore?.labels} series={salesByStore?.series} />
           <PieChartCard
             name="Pagamento"
-            labels={['Crédito', 'Débito', 'Dinheiro']}
-            series={[20, 50, 30]}
+            labels={salesByPaymentMethod?.labels}
+            series={salesByPaymentMethod?.series}
           />
         </div>
-        <SalesTable />
+        <SalesTable filterData={filterData} />
       </div>
     </>
   );
--- front-web/src/utils/request.ts
+++ front-web/src/utils/request.ts
@@ -1,6 +1,20 @@
 import axios from 'axios';
+import { FilterData } from '../types';
+import { formatDateToServer } from './formatters';
 
 const baseURL = 'http://localhost:8080';
 export const makeRequest = axios.create({
   baseURL
 });
+
+export const buildFilterParams = (
+  filterDate?: FilterData,
+  extraParams?: Record<string, unknown>
+) => {
+  return {
+    minDate: formatDateToServer(filterDate?.dates?.[0]),
+    maxDate: formatDateToServer(filterDate?.dates?.[1]),
+    gender: filterDate?.gender,
+    ...extraParams
+  };
+};
